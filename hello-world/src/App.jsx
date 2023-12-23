ca6e9ad2d752d46d076551f16fc4f9b542475424
fidexcode <fidexcode@gmail.com>
2023-12-23 03:14:45 +0100

    filtered out chemists

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,18 +1,43 @@
 import { people } from './Data.js';
 
 export default function List(){
-  const listItems = people.map(person=>
-    <li key={person.id}>
-      <p>
-        <b>{person.name}</b> 
-        { ' ' + person.profession + ' ' }
-        known for {person.accomplishment}
-      </p>
-    </li>
-    )
+
+  const chemist = people.filter(person=>
+    person.profession === 'chemist'
+    );
+
+    const everyOneElse = people.filter(person=>
+      person.profession !== 'chemist'
+      )
   return(
     <>
-    <ul>{listItems}</ul>
+    <article>
+      <h1>Scientist</h1>
+      <h2>Chemist</h2>
+      <ul>
+        {chemist.map(person=>
+        <li key={person.id}>
+          <p>
+            <b>{person.name}:</b>
+            { ' ' + person.profession + ' '}
+            known for { ' ' + person.accomplishment}
+          </p>
+        </li>
+          )}
+      </ul>
+      <h2>Every one else</h2>
+      <ul>
+        {everyOneElse.map(person=>
+        <li key={person.id}>
+          <p>{
+            <b>{person.name}:</b>}
+            { ' ' + person.profession + ' '}
+            known for {' ' + person.accomplishment}
+            </p>
+        </li>
+          )}
+      </ul>
+    </article>
     </>
   )
 }
